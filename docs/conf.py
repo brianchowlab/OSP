520eb1d9c4084918294dfb114822c87728674c72
Karol <kszymula@seas.upenn.edu>
2018-11-13 23:08:54 -0500

    Preliminary readdocs working.

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.append(os.path.abspath('../Software/Python_Files'))
 
 
 # -- Project information -----------------------------------------------------
@@ -41,6 +40,7 @@ release = u'1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
